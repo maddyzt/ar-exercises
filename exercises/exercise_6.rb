22eca837a9b732946e4fca7e2e3ce3b6c6c6efbf
Maddy Tan <maddytan7@gmail.com>
2022-06-27 19:03:48 +0000

    exercise 7 code

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,43 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+=begin
+We haven't used the Employee class (and employees table) at all yet. If you look at this table's column structure, 
+you'll find that it has a store_id (integer) column. This is a column that identifies which store each employee belongs to. 
+It points to the id (integer) column of their store.
+
+Let's tell Active Record that these two tables are in fact related via the store_id column.
+
+Add the following code directly inside the Store model (class): has_many :employees
+Add the following code directly inside the Employee model (class): belongs_to :store
+Add some data into employees. Here's an example of one (note how it differs from how you create stores): 
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+Go ahead and create some more employees using the create method. You can do this by making multiple calls to create 
+  (like you have before.) No need to assign the employees to variables though. 
+  Create them through the @store# instance variables that you defined in previous exercises. 
+  Create a bunch under @store1 (Burnaby) and @store2 (Richmond). Eg: @store1.employees.create(...).
+
+Store
+Employee
+
+  create_table :stores do |t|
+    t.column :name, :string
+    t.column :annual_revenue, :integer
+    t.column :mens_apparel, :boolean
+    t.column :womens_apparel, :boolean
+    t.timestamps null: false
+  end
+  create_table :employees do |table|
+    table.references :store
+    table.column :first_name, :string
+    table.column :last_name, :string
+    table.column :hourly_rate, :integer
+    table.timestamps null: false
+  end
+=end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "John", last_name: "Smith", hourly_rate: 60)
+@store2.employees.create(first_name: "Maddy", last_name: "Tan", hourly_rate: 100)
+@store2.employees.create(first_name: "Finn", last_name: "Tan", hourly_rate: 100)
+@store2.employees.create(first_name: "error", last_name: "error", hourly_rate: 100)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,40 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+=begin
+Add validations to two models to enforce the following business rules:
+Employees must always have a first name present
+Employees must always have a last name present
+Employees have a hourly_rate that is a number (integer) between 40 and 200
+Employees must always have a store that they belong to (can't have an employee that is not assigned a store)
+Stores must always have a name that is a minimum of 3 characters
+Stores have an annual_revenue that is a number (integer) that must be 0 or more
+BONUS: Stores must carry at least one of the men's or women's apparel (hint: use a custom validation method - don't use a Validator class)
+Ask the user for a store name (store it in a variable)
+Attempt to create a store with the inputted name but leave out the other fields (annual_revenue, mens_apparel, and womens_apparel)
+Display the error messages provided back from ActiveRecord to the user (one on each line) after you attempt to save/create the record
+  create_table :stores do |t|
+    t.column :name, :string
+    t.column :annual_revenue, :integer
+    t.column :mens_apparel, :boolean
+    t.column :womens_apparel, :boolean
+    t.timestamps null: false
+  end
+  create_table :employees do |table|
+    table.references :store
+    table.column :first_name, :string
+    table.column :last_name, :string
+    table.column :hourly_rate, :integer
+    table.timestamps null: false
+  end
+  class User < ApplicationRecord
+  validates :name, presence: true
+  end
+=end
+
+puts "Enter a store name"
+input = gets.chomp
+newStore = Store.create(name: "#{input}")
+
+puts "\nerrors:\n"
+newStore.errors.messages.each { |err, msg| puts "#{err}: #{msg}" }
