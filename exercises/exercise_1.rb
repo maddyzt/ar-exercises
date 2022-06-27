57c906e2a706f1a656acf864b904791ff419e728
Maddy Tan <maddytan7@gmail.com>
2022-06-27 18:05:44 +0000

    exercise 1 code

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,39 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+=begin
+
+Store
+Employee
+
+  create_table :stores do |t|
+    t.column :name, :string
+    t.column :annual_revenue, :integer
+    t.column :mens_apparel, :boolean
+    t.column :womens_apparel, :boolean
+    t.timestamps null: false
+  end
+  create_table :employees do |table|
+    table.references :store
+    table.column :first_name, :string
+    table.column :last_name, :string
+    table.column :hourly_rate, :integer
+    table.timestamps null: false
+  end
+
+Use Active Record's create class method multiple times to create 3 stores in the database:
+Burnaby (annual_revenue of 300000, carries men's and women's apparel)
+Richmond (annual_revenue of 1260000 carries women's apparel only)
+Gastown (annual_revenue of 190000 carries men's apparel only)
+Output (puts) the number of the stores using ActiveRecord's count method, to ensure that there are three stores in the database.
+
+=end
+
+Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+puts Store.count
+
+
+
